58a1b0527878dd27a53a73520e632b4ceca13909
Tedi Papajorgji <tedi@budware.ca>
2017-03-07 22:55:08 -0500

    Speak Test case

---
+++ src/SpeakTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Test case for the g
+ * @author Group 6
+ *
+ */
+public class SpeakTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
